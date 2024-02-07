ca601055f16674eb82904e4e214a848d45d5154e
Chase Andreasen <90057808+CAndreas7@users.noreply.github.com>
2024-02-07 11:43:18 -0600

    switched to pytest

--- tests.py
+++ tests.py
@@ -1,23 +1,25 @@
 import unittest
+import pytest
 import divide
+from divide.dividePy import dividePy
 
-class TestDividePy(unittest.TestCase):
+class TestDividePy(pytest.TestCase):
 
     def test_dividePy(self):
-        self.assertEqual(divide(10, 2), 5)
-        self.assertEqual(divide(-10, 2), -5)
-        self.assertEqual(divide(10, -2), -5)
-        self.assertEqual(divide(-10, -2), 5)
-        self.assertIsNone(divide(10, 0))
+        self.assertEqual(dividePy(10, 2), 5)
+        self.assertEqual(dividePy(-10, 2), -5)
+        self.assertEqual(dividePy(10, -2), -5)
+        self.assertEqual(dividePy(-10, -2), 5)
+        self.assertIsNone(dividePy(10, 0))
 
-class TestDivideC(unittest.TestCase):
+""" class TestDivideC(unittest.TestCase):
 
     def test_divideC(self):
         self.assertEqual(divide.divideC(10, 2), 5)
         self.assertEqual(divide.divideC(-10, 2), -5)
         self.assertEqual(divide.divideC(10, -2), -5)
         self.assertEqual(divide.divideC(-10, -2), 5)
-        self.assertIsNone(divide.divideC(10, 0))
+        self.assertIsNone(divide.divideC(10, 0)) """
 
 if __name__ == '__main__':
     unittest.main()
