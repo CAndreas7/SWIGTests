f7f8ef5e85851c1804603fa3c56c2bc033b6d17b
Chase Andreasen <90057808+CAndreas7@users.noreply.github.com>
2024-02-12 09:05:50 -0600

    mocking

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -1,10 +1,20 @@
+import pytest
 from divide import dividePy
+from main import add, multiply
 
-class TestPytest:
-    
-    def test_pytest_dividePy(self):
-        assert dividePy(10, 2) == 5
-        assert dividePy(-10, 2) == -5
-        assert dividePy(10, -2) == -5
-        assert dividePy(-10, -2) == 5
-        assert dividePy(10, 0) is None
+def test_dividePy():
+    assert dividePy(10, 2) == 5
+    assert dividePy(-10, 2) == -5
+    assert dividePy(10, -2) == -5
+    assert dividePy(-10, -2) == 5
+    assert dividePy(10, 0) is None
+
+def test_add(mocker):
+    mocker.patch('main.add', return_value=10)
+    result = add(3, 7)
+    assert result == 10
+
+def test_multiply(mocker):
+    mocker.patch('main.add', return_value=15)
+    result = multiply(3, 5)
+    assert result == 15
--- tests/test_unittest.py
+++ tests/test_unittest.py
@@ -1,5 +1,7 @@
 import unittest
 from divide import dividePy
+from main import add, multiply
+from unittest.mock import patch
 
 
 class TestUnittest(unittest.TestCase):
@@ -10,6 +12,16 @@ class TestUnittest(unittest.TestCase):
         self.assertEqual(dividePy(10, -2), -5)
         self.assertEqual(dividePy(-10, -2), 5)
         self.assertIsNone(dividePy(10, 0))
+        
+    def test_add(self):
+        with patch('main.add', return_value=10):
+            result = add(3, 7)
+            self.assertEqual(result, 10)
+
+    def test_multiply(self):
+        with patch('main.add', return_value=15):
+            result = multiply(3, 5)
+            self.assertEqual(result, 15)
 
 if __name__ == '__main__':
     unittest.main()
