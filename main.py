dc6fbaa23b4beaf244926bef7a29a35c3e9aa8aa
Chase Andreasen <90057808+CAndreas7@users.noreply.github.com>
2024-02-12 09:54:55 -0600

    more tests

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ def add(a, b) :
 
 def multiply(a, b) :
     result = 0
+    if b < 0 : b = b * -1
     for _ in range(b):
         result = add(result, a)
         
--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -1,6 +1,7 @@
 import pytest
 from divide import dividePy
 from main import add, multiply
+from unittest.mock import patch
 
 def test_dividePy():
     assert dividePy(10, 2) == 5
@@ -8,11 +9,33 @@ def test_dividePy():
     assert dividePy(10, -2) == -5
     assert dividePy(-10, -2) == 5
     assert dividePy(10, 0) is None
+    assert dividePy(0, 10) == 0
+    assert dividePy(0, -10) == 0
+    assert dividePy(0, 0) is None
 
 def test_add():
     assert add(3,7) == 10
+    assert add(-3,7) == 4
+    assert add(3,-7) == -4
+    assert add(-3,-7) == -10
 
-def test_multiply(mocker):
-    mocker.patch('main.add', return_value=15, autospec=True)
-    result = multiply(3, 5)
-    assert result == 15
+def test_multiply():
+    with patch('main.add', return_value=15) as mock_add:
+        result = multiply(3, 5)
+        assert result == 15
+        assert mock_add.call_count == 5
+
+    with patch('main.add', return_value=0) as mock_add:
+        result = multiply(0, 5)
+        assert result == 0
+        assert mock_add.call_count == 5
+        
+    with patch('main.add', return_value=-15) as mock_add:
+        result = multiply(-3, 5)
+        assert result == -15
+        assert mock_add.call_count == 5
+    
+    with patch('main.add', return_value= -6) as mock_add:
+        result = multiply(2, -3)
+        assert result == -6
+        assert mock_add.call_count == 3
