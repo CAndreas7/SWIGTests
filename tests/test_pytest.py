251e38856917429878606ad9eff15deb67127f35
Chase Andreasen <90057808+CAndreas7@users.noreply.github.com>
2024-02-12 09:25:00 -0600

    autospec to tests

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -9,12 +9,10 @@ def test_dividePy():
     assert dividePy(-10, -2) == 5
     assert dividePy(10, 0) is None
 
-def test_add(mocker):
-    mocker.patch('main.add', return_value=10)
-    result = add(3, 7)
-    assert result == 10
+def test_add():
+    assert add(3,7) == 10
 
 def test_multiply(mocker):
-    mocker.patch('main.add', return_value=15)
+    mocker.patch('main.add', return_value=15, autospec=True)
     result = multiply(3, 5)
     assert result == 15
